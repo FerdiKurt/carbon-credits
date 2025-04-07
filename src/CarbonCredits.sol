8fa613a7a3d92ba32566d880094493950fa18a32
Ferdi Kurt <ferdi.kurt111@gmail.com>
2025-04-07 21:19:42 +0300

    Helper function to convert uint to string

--- src/CarbonCredits.sol
+++ src/CarbonCredits.sol
@@ -237,4 +237,30 @@ contract CarbonCredits is ERC1155, AccessControl, Errors {
     function supportsInterface(bytes4 interfaceId) public view override(ERC1155, AccessControl) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
+    /**
+     * @dev Helper function to convert uint to string
+     */
+    function _toString(uint256 value) internal pure returns (string memory) {
+        if (value == 0) {
+            return "0";
+        }
+        
+        uint256 temp = value;
+        uint256 digits;
+        
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        
+        bytes memory buffer = new bytes(digits);
+        
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        
+        return string(buffer);
+    }
 }
